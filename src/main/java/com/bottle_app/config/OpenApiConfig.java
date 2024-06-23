2ca656bf5ae8c7538875ad6471b07788e4399179
thoon0531 <taehoon1127@gmail.com>
2024-06-23 21:55:38 +0900

    swagger domain

--- src/main/java/com/bottle_app/config/OpenApiConfig.java
+++ src/main/java/com/bottle_app/config/OpenApiConfig.java
@@ -4,9 +4,12 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @SecurityScheme(
@@ -19,7 +22,9 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenApi(@Value("${app.description})") String description,
                                  @Value("${app.version}") String version) {
+        Server server = new Server().url("https://bottle-server.site");
         return new OpenAPI()
+                .servers(List.of(server))
                 .info(new Info()
                         .title("Bottle App Server API")
                         .version(version)
